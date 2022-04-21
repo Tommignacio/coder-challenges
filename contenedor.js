0db734198a598f7cdeb182a7c93ad4f2bf14ecf8
Tommignacio <88063640+Tommignacio@users.noreply.github.com>
2022-04-21 19:15:35 -0300

    second commit

---
+++ contenedor.js
@@ -0,0 +1,103 @@
+const fs = require("fs");
+class Container {
+	constructor(nombre) {
+		this.nombre = nombre;
+	}
+
+	//lee el archivo
+	async read(obj) {
+		try {
+			let file = await fs.promises.readFile(this.nombre, "utf-8");
+			//si el archivo esta vacío
+			if (file === "") {
+				file = [];
+				file.push(obj);
+				return file;
+			}
+			//si tiene objeos
+			else {
+				let fileObj = JSON.parse(file);
+				return fileObj;
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	//escibe el archivo
+	async write(res) {
+		try {
+			await fs.promises.writeFile(this.nombre, res);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	//agrega y guarda un nuevo objeto
+	async save(obj) {
+		try {
+			//lee el archivo
+			let res = await this.read(obj);
+			//busca repetidos
+			for (let el of res) {
+				if (el["title"] === obj["title"]) throw "error, producto repetido";
+			}
+			//agrega el nuevo id
+			obj["id"] = res.length + 1;
+			res.push(obj);
+			//escribe el nuevo objeto
+			this.write(JSON.stringify(res));
+			return `el id del nuevo objeto es ${obj["id"]}`;
+		} catch (error) {
+			return error;
+		}
+	}
+
+	//devuelve el producto por su ID
+	async getById(id) {
+		try {
+			let res = await this.read();
+			for (let el of res) {
+				if (id === el["id"]) {
+					return el;
+				}
+			}
+			return null;
+		} catch (error) {
+			return error;
+		}
+	}
+
+	//devuelve tosdos los objetos
+	async getAll() {
+		try {
+			let all = await this.read();
+			return all;
+		} catch (error) {
+			return error;
+		}
+	}
+
+	//elimina objeto por su id
+	async deleteById(id) {
+		try {
+			let res = await this.read();
+			let nres = res.filter((el) => el["id"] !== id);
+			console.log(nres);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	//borra todos los objetos
+	async deleteAll() {
+		try {
+			let res = await this.read();
+			res = "";
+			this.write(res);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+}
+module.exports = Container;
---
+++ test.js
@@ -0,0 +1,21 @@
+const Container = require("./contenedor");
+
+async function test() {
+	const file = new Container("./productos.txt");
+
+	const saveNew = await file.save({
+		title: "cartulina",
+		price: 123.45,
+		thumbnail: "cartulina.jpg",
+	});
+	console.log(saveNew);
+	// let getId = await file.getById(2);
+	// console.log(getId);
+
+	// const read = await file.getAll();
+	// console.log(read);
+
+	// await file.deleteById(1);
+	// await file.deleteAll();
+}
+test();
